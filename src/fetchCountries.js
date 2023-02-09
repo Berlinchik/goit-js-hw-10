8ea5bf1aa9c94e257c8f0f6cf55c7f14ca5dcf51
Berlinchik <davidarts11@gmail.com>
2023-02-09 13:46:37 +0200

    update hw-10 (create function fetchCountries)

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export default function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+  const params = new URLSearchParams({
+    fields: 'name,capital,population,flags,languages',
+  });
+  return fetch(`${BASE_URL}${name}?${params}`).then(response => {
+    if (response.status === 404) {
+      console.log('Oops, there is no country with that name');
+    }
+    if (!response.ok) {
+      throw Error('Error!');
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-import './css/styles.css';
+// import './css/styles.css';
+import fetchCountries from './fetchCountries.js';
+// import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries('peru')
+  .then(result => {
+    if (result.length > 10) {
+      console.log('Too many matches found. Please enter a more specific name.');
+      return;
+    }
+    console.log(result[0]);
+    const {
+      name: { official: name },
+      capital: [capital],
+      population,
+      flags: { svg: flags },
+      languages,
+    } = result[0];
+  })
+  .catch(error => console.log(error));
+
+function markupRender(obj) {
+  const markup = `<svg width="70" height="70"> 
+  <use href="${obj.flags}">
+  </use></svg>
+  <h1>${name}</h1>
+  `;
+}
